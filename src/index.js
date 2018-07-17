84e9a2b1f6297a45a29189017687c3f9d87e44f4
Ovidiu Cherecheș <skidding@gmail.com>
2018-07-17 15:08:30 +0300

    Simplify var names

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { registerZoneElement as register } from './store';
+export { registerElement as register } from './store';
 export { Zone } from './Zone';
 export { MultiZone } from './MultiZone';
