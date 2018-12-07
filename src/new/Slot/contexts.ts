ab8def140408f90ea4269579b69c07a3f0679675
Ovidiu Cherecheș <skidding@gmail.com>
2018-12-07 13:05:03 +0200

    Document code

--- src/new/Slot/contexts.ts
+++ src/new/Slot/contexts.ts
@@ -8,6 +8,11 @@ interface ISlotContexts {
   [slotName: string]: React.Context<SlotContextValue>;
 }
 
+// Slots are nested, so higher-level Slots can contain lower-level Slots.
+// Multiple Plugs can be consumed by the same Slot, and the reference to the
+// current Plug is is passed down through the Slot context. A different context
+// is created for each Slot name to prevent Slots that contain other Slots from
+// interfering with each other’s Plug references.
 const slotContexts: ISlotContexts = {};
 
 export function getSlotContext(slotName: string) {
