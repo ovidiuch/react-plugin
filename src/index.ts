805804c2fa2d16059ffd3b0b650212462b941043
Ovidiu Cherecheș <skidding@gmail.com>
2019-02-03 21:43:02 +0200

    Export types

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export {
   loadPlugins,
   onStateChange,
 } from 'ui-plugin';
+export { IPlug, IPlugArgs } from './types';
 export { createPlugin } from './createPlugin';
 export { resetPlugins } from './store';
 export { Slot } from './Slot';
